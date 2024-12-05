4b257a11019a0cd2cd63bce256802d9a28a60058
jhordyess <dev@jhordyess.com>
2024-12-04 22:35:41 -0400

    Implement password validation form with user feedback and styling

--- yarn/packages/vanilla-app/src/main.ts
+++ yarn/packages/vanilla-app/src/main.ts
@@ -1,1 +1,55 @@
 import './style.css'
+import { countNumbers, countSpecialChars } from '@common/validator'
+
+const Form = document.getElementById('passwordForm')
+const passwordInput = document.getElementById('password')
+const confirmPasswordInput = document.getElementById('password2')
+const messageDiv = document.getElementById('message')
+
+const isFormElement = Form instanceof HTMLFormElement
+const isPasswordInputElement = passwordInput instanceof HTMLInputElement
+const isConfirmPasswordInputElement = confirmPasswordInput instanceof HTMLInputElement
+const isMessageDivValid = messageDiv instanceof HTMLDivElement
+
+const isAllValid =
+  isFormElement && isPasswordInputElement && isConfirmPasswordInputElement && isMessageDivValid
+
+if (isAllValid)
+  Form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const passwordValue = passwordInput.value
+    const password2Value = confirmPasswordInput.value
+
+    messageDiv.textContent = ''
+
+    try {
+      if (passwordValue !== password2Value) {
+        throw new Error('❌ Passwords do not match.')
+      }
+
+      if (passwordValue.length < 7) {
+        throw new Error('❌ Password must be at least 7 characters long.')
+      }
+
+      const spChars = countSpecialChars(passwordValue)
+
+      if (spChars < 2) {
+        throw new Error('❌ Password must contain at least 2 special characters.')
+      }
+
+      const numbers = countNumbers(passwordValue)
+
+      if (numbers < 2) {
+        throw new Error('❌ Password must contain at least 2 numbers.')
+      }
+
+      messageDiv.textContent = '✅ Password is valid.'
+      messageDiv.classList.remove('text-red-500')
+      messageDiv.classList.add('text-green-500')
+    } catch (error) {
+      messageDiv.textContent = (error as Error)?.message || '❌ Something went wrong.'
+      messageDiv.classList.remove('text-green-500')
+      messageDiv.classList.add('text-red-500')
+    }
+  })
