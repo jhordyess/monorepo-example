30959f615e943da8082ef9f4b1e9473474dd7751
jhordyess <dev@jhordyess.com>
2024-12-04 22:24:49 -0400

    Update webpack configuration to output ES module format and enable module experiments

--- yarn/packages/validator/webpack.config.cjs
+++ yarn/packages/validator/webpack.config.cjs
@@ -1,9 +1,9 @@
-/** @type {import('webpack').Configuration} */
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
 const NODE_ENV = process.env.NODE_ENV
 
+/** @type {import('webpack').Configuration} */
 module.exports = {
   name: 'validator',
   entry: path.resolve('index.ts'),
@@ -12,10 +12,13 @@ module.exports = {
   externals: [nodeExternals()],
   output: {
     path: path.resolve('dist'),
-    filename: '[name].cjs',
+    filename: '[name].js',
     library: {
-      type: 'commonjs'
+      type: 'module'
     }
+  },
+  experiments: {
+    outputModule: true
   },
   resolve: {
     extensions: ['.ts']
