07624eb6074ebc621b6cabeb4237832ae31673c7
jhordyess <dev@jhordyess.com>
2024-12-04 20:24:49 -0400

    Add Webpack configuration for read-input and validator packages, update package.json scripts, and ignore dist directory

---
+++ yarn/packages/validator/webpack.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('webpack').Configuration} */
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+const NODE_ENV = process.env.NODE_ENV
+
+module.exports = {
+  name: 'validator',
+  entry: path.resolve('index.ts'),
+  target: 'node',
+  mode: NODE_ENV,
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].cjs',
+    library: {
+      type: 'commonjs'
+    }
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
